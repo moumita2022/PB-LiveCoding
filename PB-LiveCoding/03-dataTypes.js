677fb70feb03da8772955dcab577fd6494b4a3ed
nrcool <nomanalam7214@gmail.com>
2022-04-19 11:53:45 +0200

    data types in js

---
+++ PB-LiveCoding/03-dataTypes.js
@@ -0,0 +1,45 @@
+// data : information / anything which is meaningfull to machine is called data
+
+// data types in JS
+// there are two main type in js
+// Primitive
+// Object
+
+//Primitive type
+// String 
+// textual data within the double quotations or single quotations or backticks;
+// "Hello world" '123' `7s%&8d5asdf`
+
+// Number
+// numeric value 
+// integer 32 
+// float   32.145   decimal number
+// + infinity 4/0;
+// - infinity -4/0;
+// NaN
+/* console.log(Number("a23")) */
+
+// Boolean
+// true or false 
+// let isStudent = false;
+// let isMarried = true;  
+
+// undefined (empty value)
+// let firstName;
+// when you declare a variable without value ,it will get undefined value
+
+// null
+// when you set empy value to variable
+// let name = null;
+
+// Symbol
+// unique value
+/* let a = Symbol("a")
+let b = Symbol("a") */
+
+
+
+// Object
+// Array 
+// object
+// function
