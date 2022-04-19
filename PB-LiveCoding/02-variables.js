21b1cbf16ee9d09cdf8892dd40fc7875b140f571
nrcool <nomanalam7214@gmail.com>
2022-04-19 11:27:06 +0200

    variables in js

---
+++ PB-LiveCoding/02-variables.js
@@ -0,0 +1,47 @@
+// variable 
+// varaible are like a container holding value within the memory.
+
+//three different ways to define variable
+
+// var firstName= "John"; (avoid using var)
+// let firstName="John";
+// const firstName="John";
+
+//execution of code in js is from top to bottom and from left to right
+
+/* var lastName = "Smith";
+var lastName = "Naqvi";
+console.log(lastName) */
+
+//3 case of defining varaible
+// camelCase 
+// let lastName = "Smith";
+// snake_case
+// let first_name= "Robert";
+// PascalCase
+// let LastName="Joseph";
+
+
+// varaible name should start with characters/alphabet ,$ and _ ; 
+
+/* let $name= "Ali";
+let _name= "Ali";
+let name = "Ali"; */
+
+
+// defining a varaible  // = assignment operator // value
+let lastName = "Smith"; // it will not allow you to redefine your varaible but you can reassign value to it.
+let firstName; //undefined
+
+console.log(lastName);
+
+lastName = "Naqvi"
+console.log(lastName);
+firstName="Syed";
+console.log(firstName);
+
+
+//const constant // something which is not changable;
+const age=2; // always use const unless you are sure that the value get changed in the future
+
+
