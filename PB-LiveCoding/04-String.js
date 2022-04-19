c31da85b7e014ffc8a02b5af4750ea65ddecd87e
nrcool <nomanalam7214@gmail.com>
2022-04-19 12:12:33 +0200

    string contatination

---
+++ PB-LiveCoding/04-String.js
@@ -0,0 +1,15 @@
+
+let firstName="Misbah"; //string
+let age = 23;// number
+let isStudent=true; //boolean
+
+//concatination
+console.log("con" + "cati" + "nation"); 
+console.log(2 + 3);
+
+console.log("My name is "+ firstName + " and I am " + age  +  " years old ")
+
+//coercion
+/* console.log("2" + 3);
+console.log(2 + "3"); */
+
