abae8a1b75325f629ee757dfdbe505789ae55269
nrcool <nomanalam7214@gmail.com>
2022-04-20 10:36:19 +0200

    conditional and logical operators

--- PB-LiveCoding/05-operators.js
+++ PB-LiveCoding/05-operators.js
@@ -15,7 +15,7 @@ console.log(10%3); // 1    10 - 9 = 1  6 % 5 = 1
 console.log(2**3); //2*2*2  = 8 */
 
 
-let a = 2; 
+/* let a = 2; */ 
 //post increment operator
 /* console.log(a++);
 console.log(a) */
@@ -26,7 +26,7 @@ console.log(a) // 1 */
 
 // pre increment,  pre decrement
 
-console.log(++a); //3
+/* console.log(++a); */ //3
 
 
 //urany operator , binary operator
@@ -37,15 +37,57 @@ console.log(++a); //3
 
 //unary operators need only one operand: ++ -- typeof
 
-console.log(typeof "hello"); 
+/* console.log(typeof "hello"); 
 console.log(typeof true);
 console.log(typeof 45);
 console.log(typeof undefined);
-console.log(typeof null); //object
+console.log(typeof null); //object */
 
 
 //convert any number into string or any string into a number
-let num = 23;
+/* let num = 23;
 console.log(String(num));
 let stringNum = "34";
-console.log(Number(stringNum));
+console.log(Number(stringNum)); */
+
+
+
+//// conditional operator 
+// < , > ,<=, >=, == abstract comparison , === strict comparison
+
+/* console.log(1<2); //true
+console.log(1>2); //false
+
+console.log(2<=2); //true
+console.log(3>=3); //true
+console.log(3 == "3"); //javascript willn't check datatype
+console.log(3 === "3"); //false ,javascript will check datatype */
+
+let sum = 2+5; // statement
+//statement : instruction to javascipt to do something
+
+//expresssion :value itself or anything will become a value is called expression;
+// "hello " + " World "  => hello World
+// 1+2+3+4  => 10 Arithmetic expresssion
+// 1 < 2  => conditional expression
+// 1<2 && 2>1 => logical expression
+
+
+
+//logical operators
+// && AND * ,  || OR +
+// && 0 1 => 0, 
+//    1 0 => 0,
+//    1 1 => 1,
+//    0 0 => 0
+/* 1<2   and  4>5  */
+console.log(1<2 && 4>5 && 2>3)   // false  => 1 * 0 * 0 = 0
+
+console.log(1<2 || 4>5 || 2>3)   // true  => 1 + 0 + 0 = 1
+
+
+
+/* const experience = 1; 
+const degree = true; 
+
+console.log(experience >=1 || degree===true) */
