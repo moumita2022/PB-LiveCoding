89d1c9a46b9551d88823aa1788fd3a956c2b746b
nrcool <nomanalam7214@gmail.com>
2022-04-26 11:56:08 +0200

    if-else.js

---
+++ PB-LiveCoding/14-f-Else.js
@@ -0,0 +1,36 @@
+// decision in Javascript
+
+// global scope
+// local / block scope / functional scope
+// lexical scope
+
+
+//Block
+/* if(2>1){
+        console.log("Javascript");
+        console.log("first Block")
+        const firstName= "Syed"
+        console.log("block");
+        console.log(firstName)
+}else{
+    console.log("second Block")
+}
+ */
+
+const age = 39; 
+//age is in between 13-19
+
+if(age<12){
+    console.log("Maria is a little girl")
+}else if(age>12 && age<20){
+    console.log("Maria is a teen ager")
+}else if(age>=20 && age<40){
+    console.log("Maria is a young woman")
+}else {
+    console.log("Maria is an old woman")
+}
+
+
+
+
+
