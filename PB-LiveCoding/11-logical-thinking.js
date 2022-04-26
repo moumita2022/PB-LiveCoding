b2f7933195d5cfce5e51df80e1a6e90ac8e9e11f
nrcool <nomanalam7214@gmail.com>
2022-04-26 09:27:47 +0200

    logical thinking

---
+++ PB-LiveCoding/11-logical-thinking.js
@@ -0,0 +1,23 @@
+// Logical Thinking
+
+// input => algorithm => output
+
+// Algorithm 
+// sequence of instructions to achieve clearly defined goal.
+
+// bake a cake
+
+// ingredient => recipe => cake
+// ingredient .... wheat, eggs, milk, ........
+
+// 1. 500 grams wheat , 2 eggs 
+// ... 
+// ...
+// ... 
+// . pre heated oven ... check if time is less than 20 mins ,you wait otherwise you take out your cake from oven 
+
+// cake is ready
+
+
+
+
