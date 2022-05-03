481acb14a190567ca9d2b8522503c3e9f7ce6325
nrcool <nomanalam7214@gmail.com>
2022-05-03 13:47:56 +0200

    hoisting

---
+++ PB-LiveCoding/19-Hoisting.js
@@ -0,0 +1,23 @@
+// Hoisting
+// Hoisting is JavaScript's default behavior of moving all declarations to the top of the current scope (to the top of the current script or the current function).
+
+//hoisting allows you to use your varaible before declaration:
+
+
+ // declaration  // initialization
+/* let name = "Naqvi"; 
+
+function showNumber(){}
+
+
+const showNumber = function(){} */
+
+
+
+/* printMessage() */
+/* 
+const printMessage = ()=>{
+    console.log("hello Syed")
+}
+ */
+
