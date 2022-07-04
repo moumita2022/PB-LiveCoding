2ccc69a266afde117f6df59f9b30eb4be3680f16
nrcool <nomanalam7214@gmail.com>
2022-07-04 11:24:51 +0200

    mouse events ,  browser event

--- SPA-LiveCoding/DOM-Events/index.js
+++ SPA-LiveCoding/DOM-Events/index.js
@@ -1,6 +1,21 @@
 let h1 = document.querySelector("h1")
 
 
+//load DOMContentLoaded
+
+/* window.addEventListener("load",()=>{
+    //trigger when html DOM is created
+    console.log("page loaded")
+    document.body.style.backgroundColor="green"
+}) */
+
+window.addEventListener("DOMContentLoaded",()=>{
+    //trigger when content in dom loaded
+    console.log("DOM loaded successfully ")
+})
+
+// click change input keyup submit resize load DOMContentLoaded
+
 
 //first way
 /* h1.onclick = function(){
@@ -50,5 +65,35 @@ h1.addEventListener("click",clickhandler)
 },5000) */
 
 
-h1.addEventListener("")
+
 // mouseover // mouseenter  // mouseleave  //mouseout //click  //dblclick //contextmenu
+
+//target second paragraph
+let secondPara = document.querySelector(".para2")
+
+secondPara.style.border = "2px solid black"
+secondPara.style.padding="20px"
+
+/* secondPara.addEventListener("mouseover", ()=>{
+    secondPara.style.backgroundColor="red"
+    secondPara.style.color="white"
+} )
+
+secondPara.addEventListener("mouseout",()=>{
+    secondPara.style.backgroundColor=""
+    secondPara.style.color="black"
+}) */
+
+/* secondPara.addEventListener("dblclick", ()=>{
+    h1.style.backgroundColor="red"
+    h1.innerText= "user double clicked on second paragraph"
+}) */
+
+/* secondPara.addEventListener("contextmenu", ()=>{
+    alert("right click is not allowed")
+}) */
+
+
+/* secondPara.addEventListener("dragstart",()=>{
+    console.log("do something")
+}) */
