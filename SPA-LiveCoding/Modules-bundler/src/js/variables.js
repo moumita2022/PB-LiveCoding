b186e50e3c3c8c0506b410e9aa294aee00f0ea93
nrcool <nomanalam7214@gmail.com>
2022-07-12 11:36:15 +0200

    parcel bundler

---
+++ SPA-LiveCoding/Modules-bundler/src/js/variables.js
@@ -0,0 +1,16 @@
+//targeted all three input fields
+/* export const number1 = document.getElementById("num1")
+export const number2= document.getElementById("num2") 
+export const result = document.getElementById("result") */
+
+const number1 = document.getElementById("num1")
+const number2= document.getElementById("num2") 
+const result = document.getElementById("result")
+
+export  { number1,number2,result } 
+
+
+
+
+
+
